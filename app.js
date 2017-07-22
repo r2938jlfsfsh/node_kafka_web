78dec583b3dd65aa4b3d76583043a537ea318804
Russell Russell <russell_g1974@yahoo.com.au>
2017-07-22 16:55:23 +1000

    Updating html

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 var express = require('express')
     , http = require('http')
-    , sseMW = require('./sse');
+    , sseMW = require('./sse')
+    , conf = require('./config');
 
 var kafka = require('kafka-node');
 var Consumer = kafka.Consumer;
-var client = new kafka.Client("localhost:32181/"); //Note this is zookeeper, not broker
-var countriesTopic = "node_test";
+var client = new kafka.Client(conf.ZOOKEEPER_CONN);
 
 var app = express();
 var server = http.createServer(app);
 
-var PORT = process.env.PORT || 3000;
-var APP_VERSION = '0.9';
-
-server.listen(PORT, function () {
-    console.log('Server running, version '+APP_VERSION+', Express is listening... at '+PORT+" ");
+server.listen(conf.LISTEN_PORT, function () {
+    console.log('Server running, version '+conf.APP_VERSION+', Express is listening... at '+ conf.LISTEN_PORT);
 });
 
 // Realtime updates
@@ -23,7 +20,7 @@ var sseClients = new sseMW.Topic();
 app.use(express.static(__dirname + '/public'));
 app.get('/about', function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("Version "+APP_VERSION+". No Data Requested, so none is returned");
+    res.write("Version "+conf.APP_VERSION+". No Data Requested, so none is returned");
     res.write("Supported URLs:");
     res.write("/public , /public/index.html ");
     res.write("incoming headers" + JSON.stringify(req.headers));
@@ -34,13 +31,14 @@ app.get('/about', function (req, res) {
 // Connection property to the request
 app.use(sseMW.sseMiddleware)
 
+// TODO Make this generic, not just jobStatus - should have one for each supported topic\
+// TODO Or it should be one but take a list of topics to follow
 // initial registration of SSE Client Connection
-app.get('/topn/updates', function(req,res){
+app.get('/jobStatus/updates', function(req,res){
     var sseConnection = res.sseConnection;
     sseConnection.setup();
     sseClients.add(sseConnection);
 } );
-
 
 var m;
 //send message to all registered SSE clients
@@ -60,7 +58,7 @@ updateSseClients = function(message) {
 initHeartbeat = function(interval) {
     setInterval(function()  {
             var msg = {"label":"The latest", "time":new Date()};
-            updateSseClients(msg);// JSON.stringify(msg));
+            updateSseClients(msg);
         }//interval function
         , interval?interval*1000:3000
     ); // setInterval
@@ -69,7 +67,7 @@ initHeartbeat = function(interval) {
 // initialize heartbeat at x second interval
 initHeartbeat(30);
 
-// Configure Kafka Consumer for Kafka Top3 Topic and handle Kafka message (by calling updateSseClients)
+// Configure Kafka Consumer
 var consumer = new Consumer(
     client,
     [],
@@ -77,20 +75,23 @@ var consumer = new Consumer(
 );
 
 consumer.on('message', function (message) {
-    handleCountryMessage(message);
+    handleMessage(message);
 });
 
 consumer.on('error', function(err) {
     console.log("ERROR: " + err.message);
 });
 
-
 consumer.addTopics([
-    { topic: "node_test", partition: 0, offset: 0}
-], () => console.log("topic added"));
+    { topic: conf.kafkaTopic, partition: 0, offset: 0}
+], () => console.log("Topic added: " + conf.kafkaTopic));
 
-function handleCountryMessage(countryMessage) {
-    var top3 = JSON.parse(countryMessage.value);
-    top3.continent = new Buffer(countryMessage.key).toString('ascii');
-    updateSseClients( top3);
-}// handleCountryMessage
+function handleMessage(msg) {
+    //var top3 = JSON.parse(msg.value);
+    //top3.continent = new Buffer(msg.key).toString('ascii');
+    //updateSseClients( top3);
+    //console.log("Full msg: "+JSON.stringify(msg));
+    var outMsg = {topic: msg.topic, value: msg.value};
+    //console.log("Output msg: "+JSON.stringify(outMsg));
+    updateSseClients(outMsg);
+}// handleMessage
